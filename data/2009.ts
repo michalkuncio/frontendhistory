a8f23a768a2fe4b4c354742182240205d736cd43
Carlos Giner <carlosginer90@gmail.com>
2023-12-18 07:51:15 +0100

    Add node js 0.0.3

---
+++ data/2009.ts
@@ -0,0 +1,11 @@
+export default {
+    year: 2009,
+    items: [
+        {
+            date: '2009-06-11',
+            title: 'Node.js first public release (version 0.0.3)',
+            link: 'https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_ARCHIVE.md#0.0.3',
+            icon: 'node'
+        }
+    ]
+};
