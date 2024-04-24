da970dcc9f1b3ac62ba19608641ad3f88a24c2f9
Daniel Roe <daniel@roe.dev>
2024-04-24 13:06:21 +0100

    fix: ignore errors and add root element to `index.vue`

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,37 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt({
-    rules: {
-        'vue/html-indent': ['error', 4],
-        'vue/html-self-closing': ['error', {
-            html: {
-                void: 'never',
-                normal: 'always',
-                component: 'always'
-            },
-            svg: 'always',
-            math: 'always'
-        }],
-        'vue/max-attributes-per-line': ['error', {
-            singleline: { max: 5 },
-            multiline: { max: 1 }
-        }],
-        'no-console': 'off'
+export default withNuxt(
+    {
+        rules: {
+            'vue/html-indent': ['error', 4],
+            'vue/html-self-closing': ['error', {
+                html: {
+                    void: 'never',
+                    normal: 'always',
+                    component: 'always'
+                },
+                svg: 'always',
+                math: 'always'
+            }],
+            'vue/max-attributes-per-line': ['error', {
+                singleline: { max: 5 },
+                multiline: { max: 1 }
+            }],
+            'no-console': 'off'
+        }
+    },
+    // TODO: Move these files to multi-word component names
+    {
+        files: [
+            'components/Faq.vue',
+            'components/Footer.vue',
+            'components/Header.vue',
+            'components/Hero.vue',
+            'components/Nav.vue',
+            'components/Roadmap.vue'
+        ],
+        rules: {
+            'vue/multi-word-component-names': 'off'
+        }
     }
-});
+);
